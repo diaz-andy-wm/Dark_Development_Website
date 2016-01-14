12416da25c5f6137915404078faf68cb88617157
Start Coding Student 1 <session1@START-Coding-MBP-09.local>
2016-01-14 13:36:53 -0700

    Revert "don't pull this"
    This reverts commit 20f139ea8f834809ffaaaafb436fd6bcd41f05f2.

---
+++ home.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+    <script src="jquery copy.js"></script>
+    <link rel="stylesheet" type="text/css" href="navStyle.css">
+    <title></title>
+</head>
+<body>
+<header id="topnav" style="z-index: 99">
+    <div class="inner">
+        <div class="navLogo"><img src="pics/logo.png"> </div> <div class="name"><h1 style="font-size: 21px;">Dark Development</h1></div>
+        <nav role='navigation'>
+            <ul>
+                <li><a href="home.html">Home</a></li>
+                <li><a href="aboutUs.html">About</a></li>
+                <li><a href="newProducts.html">Products</a></li>
+                <li><a href="projCommunity.html">Member Projects</a></li>
+                <li><a href="profilePage.html">Profile</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<div class="container"><!--Container -->
+
+
+    <div class="jumboSlide" ><!-- Jumbo slide-->
+
+        <div class="container" style="background-color:#D1D1D1;">
+
+            <div id="content-slider"><!-- content-slider-->
+
+                <div id="slider">  <!-- Slider container -->
+
+                    <div id="mask">  <!-- Mask -->
+
+                        <ul>
+                            <li id="first" class="firstanimation">
+                                <!--canon silver-->
+                                <div id="s-pic1"><img src="http://www.stonepower.fr/news/wp-content/uploads/2012/10/AE1.png"></div>
+                                <div class="tooltip"> </div>
+
+                            </li>
+
+                            <li id="second" class="secondanimation">
+                                <!--ocean photo with guy and camera tripod-->
+
+                                <div id="s-pic2"><img src="https://d2ieyhi8galmxj.cloudfront.net/uploads/newsletters/image/newsite-slider/welcomeSlide-image-0.jpg"/></div>
+                                <div class="tooltip"></div>
+
+                            </li>
+
+                            <li id="third" class="thirdanimation">
+                                <!--nikon fall-->
+                                <div id="s-pic3"><img src="http://cdn.desktopwallpapers4.me/wallpapers/photography/1920x1200/2/20211-nikon-camera-1920x1200-photography-wallpaper.jpg"/></div>
+                                <div class="tooltip"></div>
+
+                            </li>
+
+                            <li id="fourth" class="fourthanimation">
+                                <!--close up-->
+                                <div id="s-pic4"><img src="http://www.dailypicsupdate.com/wp-content/uploads/2013/03/camera-wallpaper-free-wallpaper-hd.jpg"/></div>
+                                <div class="tooltip"></div>
+
+                            </li>
+
+                            <li id="fifth" class="fifthanimation">
+                                <!--sinlge person and camera-->
+                                <div id="s-pic5"><img src="http://www.magic4walls.com/wp-content/uploads/2014/03/camera-lens-station-photo-nikon-girl-photographer-hd-wallpaper.jpg"/></div>
+                                <div class="tooltip"></div>
+
+                            </li>
+                        </ul>
+
+                    </div>  <!-- End Mask -->
+
+                </div>  <!-- End Slider Container -->
+
+            </div>
+
+        </div>
+<<<<<<< HEAD:home.php
+        <!-- red search button (not functional yet)-->
+        <div class="nav-bar-menu"><!-- nav-bar-menu start-->
+            <div id="logo">
+                <img src="pics/logo.png" style="width:57px;height:50px;margin-top:9px;margin-left:5px;">
+                <span style="font-family: Vato, Verdana, sans-serif;-webkit-font-smoothing: antialiased;font-weight: 400;font-size:21px;">Dark Development</span>
+            </div>
+
+            <div class="search-drop-down"><!-- Start horizontal navigation bar-->
+
+                <div id="text-search">Search Bar:</div>
+
+            </div>
+
+            <div class="search-menu" style="font-family: Vato, Verdana, sans-serif">
+                <!--Horizontal nav bar -->
+                <ul>
+
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="aboutUs.php">About</a></li>
+                    <li><a href="newProducts.php">Products</a></li>
+                    <li><a href="projCommunity.php">Member Projects</a></li>
+                    <li><a href="profilePage.php">Profile</a></li>
+
+                </ul>
+
+
+            </div>
+            <!-- End horizontal navigation bar-->
+=======
+>>>>>>> origin/gavinV3:home.html
+
+
+    </div><!--jumbo slide end -->
+
+
+    <div class="filler">
+
+        <div class="pic-title"> <!-- Picture gallery title-->
+            <h1 style="text-shadow: 2px 1px #931D1F">Hover Over Pictures For More</h1>
+        </div>
+        <div id="filler" style="margin-top:-125px;background-color:black;width:4px;height:1378px;position:relative;margin-left:-4px;margin-right:1000px;"></div>
+
+        </div>
+    <!-- Gallery Start-->
+
+    <div class="background-color"> <!--Picture Gallery (future idea, on next iteration probably) viewers would be able to hover over the pictures and show more information
+    about the thing they hover over, above the image and be able to click on it to see what we have of that product, costs, rentals, and e.t.c.-->
+
+        <div class="pic-gallery">
+
+
+            <div id="pic-1" class="pics"></div>
+
+            <div id="pic-2" class="pics"></div>
+
+            <div id="pic-3" class="pics"></div>
+
+            <div id="pic-4" class="pics"></div>
+
+            <div id="pic-5" class="pics"></div>
+
+            <div id="pic-6" class="pics"></div>
+
+            <div id="pic-7" class="pics"></div>
+
+            <div id="pic-8" class="pics"></div>
+
+            <div id="pic-9" class="pics"></div>
+
+        </div>
+
+
+        </div>
+    <div id="filler2" style="margin-top:2px;background-color:black;width:1424px;height:3px;position:relative;margin-left:-5px;"></div>
+
+    <div style="height:150px; width:3px;background-color:black; margin-left: 470px;"> <!-- Vertical break black line 1-->
+
+        <div style="height:150px; width:3px;background-color:black; margin-left: 460px; margin-bottom:60px;"><!--  Vertical break black line 2-->
+
+            <div id="contact-us"> <!-- COntact information div -->
+
+                <h3>Any Questions? <br>Contact us!</h3>
+
+        <ul>
+
+            <li>Mollee(CEO):123-456-7890</li>
+
+            <li>Andy(CEO):123-456-7890</li>
+
+            <li>Gavin(CEO):123-456-7890</li>
+
+        </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="copyright">
+            <p>&copy 2015 - Dark Development,Inc. All Rights Reserved</p>
+        </div>
+        <div class="logo">
+            <img src="http://images.clipartpanda.com/camera-flash-silhouette-camera-md.png">
+        </div>
+        <div class="links">
+            <a href="home.php">Home</a>
+            <a href="index.php">Sign-In</a>
+            <a href="aboutUs.php">About Us</a>
+        </div>
+    </footer>
+
+    <!-- Gallery End-->
+
+
+    </div>
+
+
+
+
+</body>
+</html>
